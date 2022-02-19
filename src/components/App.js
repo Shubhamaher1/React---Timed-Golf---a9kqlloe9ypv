07e05faf646617f463b0ea19052f1dc7797877f8
Shubham  Aher <84302530+Shubhamaher1@users.noreply.github.com>
2022-02-19 18:49:12 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { time: 0, x: 0, y: 0 };
+  }
+  componentDidMount() {
+    
+  }
+
+  componentWillUnmount() {
+    
+  }
+
+
+
+  render() {
+    return (
+ <>
+</>
+    );
+  }
+}
+
+export default Timer;
